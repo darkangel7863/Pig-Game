a731991af25e3cb242e101e910db17ccc0afee56
Mohamed Ismail <darkangel7863@gmail.com>
2025-12-10 10:24:23 +0200

    Rolling the dice

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 // current
 // current-label
 // current-score
-// dice
 // btn
 // btn--new
 // btn--roll
@@ -14,9 +13,31 @@
 // Selecting Elements
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+
+let currentScore0 = 0;
+let currentScore1 = 0;
+
+const btnNewEl = document.querySelector('.btn--new');
+const btnRollEl = document.querySelector('.btn--roll');
+const btnHoldEl = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling dice
+btnRollEl.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore0 += dice;
+    current0El.textContent = currentScore0;
+  } else {
+  }
+});
