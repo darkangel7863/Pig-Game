29ae1f2a7b61629f3efb3360abf9da0faa1eeff7
Mohamed Ismail <darkangel7863@gmail.com>
2025-12-10 10:54:55 +0200

    updating the players

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 'use strict';
 // player player--0 player--active
-// name
 
-// current
-// current-label
-// current-score
 // btn
 // btn--new
 // btn--roll
 // btn--hold
 
 // Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 
-let currentScore0 = 0;
-let currentScore1 = 0;
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 const btnNewEl = document.querySelector('.btn--new');
 const btnRollEl = document.querySelector('.btn--roll');
@@ -36,8 +36,14 @@ btnRollEl.addEventListener('click', function () {
   diceEl.src = `dice-${dice}.png`;
 
   if (dice !== 1) {
-    currentScore0 += dice;
-    current0El.textContent = currentScore0;
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
