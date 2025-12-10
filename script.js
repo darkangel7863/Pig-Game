7bd5b10b45e59bfa2d3e399e8c35bed8113d1579
Mohamed Ismail <darkangel7863@gmail.com>
2025-12-10 09:48:28 +0200

    creating the starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+// player player--0 player--active
+// name
+
+// current
+// current-label
+// current-score
+// dice
+// btn
+// btn--new
+// btn--roll
+// btn--hold
+
+// Selecting Elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
